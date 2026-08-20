adbd4dcb987919c19f973ff89ab95c4abda4ddf6
Aaisha <79445507+AaishaDev@users.noreply.github.com>
2026-08-20 12:24:09 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let chars = s.toLowerCase().replace(/[^a-z0-9]/g, '').split('')
+        for(let i = 0; i<chars.length ; i++){
+            if(chars[i] === chars[chars.length - (i+1)]) continue ;
+            else return false
+        }
+        return true
+    }
+}
