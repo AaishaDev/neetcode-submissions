e7028b23fa524b597a5c2d222d8c4dbf5dd0e95a
Aaisha <79445507+AaishaDev@users.noreply.github.com>
2026-08-21 13:03:16 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let min = prices[0];
+        let profit = 0;
+        for (let i = 1; i < prices.length; i++) {
+            if (min < prices[i]) {
+                profit < prices[i] - min ? profit = prices[i] - min : ''
+            }
+            else{
+                min = prices[i]
+            }
+        }
+        return profit
+    }
+}
