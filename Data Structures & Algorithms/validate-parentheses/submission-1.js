92bf6668b245146a4c02e94feb79bcb590410e3a
Aaisha <79445507+AaishaDev@users.noreply.github.com>
2026-08-22 23:54:37 +0530

    Update: validate-parentheses - submission-1

--- Data Structures & Algorithms/validate-parentheses/submission-1.js
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -5,23 +5,25 @@ class Solution {
      */
     isValid(s) {
         let stack = [];
-        let brackets ={
-        '}' : '{',
-        ']' : '[',
-        ')' : '('
+
+        let brackets = {
+            '}': '{',
+            ']': '[',
+            ')': '('
         }
-        for(const idx in s){
-            if(s[idx] === '[' || '{' , '(')
-            {
+
+        for (const idx in s) {
+            if (s[idx] === '[' || s[idx] === '{' || s[idx] === '(') {
                 stack.push(s[idx])
-            }
-            else{
-                if(brackets[s[idx]] == stack[s.length - idx]){
+            } else {
+                if (brackets[s[idx]] == stack[stack.length - 1]) {
                     stack.pop()
+                } else {
+                    return false
                 }
-                else return false
             }
         }
-        return true
+
+        return stack.length === 0
     }
 }
