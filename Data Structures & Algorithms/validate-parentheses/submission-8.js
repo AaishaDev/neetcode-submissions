169bf0e4fc50ad31055939582ea90b4d128f01d8
Aaisha <79445507+AaishaDev@users.noreply.github.com>
2026-08-22 23:48:28 +0530

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        if(s.length<2) return false
+        let stack = [];
+        let brackets ={
+        '}' : '{',
+        ']' : '[',
+        ')' : '('
+        }
+        for(const idx in s){
+            if(s[idx] === '[' || s[idx] === '{' || s[idx] === '(')
+            {
+                stack.push(s[idx])
+            }
+            else{
+                if(brackets[s[idx]] == stack[stack.length - 1]){
+                    stack.pop()
+                }
+                else return false
+            }
+        }
+        return true
+    }
+}
