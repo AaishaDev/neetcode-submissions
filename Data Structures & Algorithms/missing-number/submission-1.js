b012618f17078227a1ea99333a7bf5e1b5b16030
Aaisha <79445507+AaishaDev@users.noreply.github.com>
2026-08-25 14:51:41 +0530

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    missingNumber(nums) {
+        nums.sort((a, b) => a - b);
+
+        for (let i = 0; i < nums.length; i++) {
+            if (i !== nums[i]) {
+                return i
+            }
+        }
+    }
+}
